42a1fa47bddd52f224f5933f1dd64dc91368a23a
hbengaid <hicheme20@hotmail.fr>
2022-05-10 00:26:23 +0200

    mtn traduire les fct javascript en c# et afficher avec pictureBox

---
+++ BE/SystemeLocalisation/SystemeLocalisation/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SystemeLocalisation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 277);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ BE/SystemeLocalisation/SystemeLocalisation/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SystemeLocalisation
+{
+    public partial class Form1 : Form
+    {
+        MqttClient mqttClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mqttClient = new MqttClient("test.mosquitto.org");
+            mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            string clientId = Guid.NewGuid().ToString();
+            mqttClient.Connect(clientId);
+            mqttClient.Subscribe(new string[] { "tags" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            if (mqttClient.IsConnected)
+            {
+                Console.WriteLine("nique zebi");
+                label1.Text = "nique zebi";
+            }
+            else
+            {
+                label1.Text = "nique pas zebi";
+            }
+
+
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            // handle message received
+            var message = Encoding.UTF8.GetString(e.Message);
+            //var todo = JsonConvert.DeserializeObject<JObject>(message);
+            //Console.WriteLine(todo);
+            //Console.WriteLine(todo.GetType());
+            //Console.WriteLine(message);
+            List<Result> result = JsonConvert.DeserializeObject<List<Result>>(message);
+            foreach (var item in result)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(item.tagId);
+                Console.WriteLine(item.timestamp);
+                Console.WriteLine(item.data.coordinates.x);
+                Console.WriteLine(item.data.coordinates.y);
+                Console.WriteLine("-----------------------");
+            }
+        }
+
+
+        public class Coordinates
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+
+        public class Data
+        {
+            public Coordinates coordinates;
+        }
+        public class Result
+        {
+            public Data data { get; set; }
+            public int tagId { get; set; }
+            public float timestamp { get; set; }
+        }
+    }
+
+}
