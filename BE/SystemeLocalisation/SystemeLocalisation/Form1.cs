55790f3f130b426b8667f46d41dcb379c2d3959e
hbengaid <hicheme20@hotmail.fr>
2022-05-11 17:23:17 +0200

    tout ok mtn reste plus que a tracer les points sur le graphics et aussi faire algo chemin le plus court

--- BE/SystemeLocalisation/SystemeLocalisation/Form1.cs
+++ BE/SystemeLocalisation/SystemeLocalisation/Form1.cs
@@ -77,7 +77,7 @@ namespace SystemeLocalisation
         private void Form1_Load(object sender, EventArgs e)
         {
             //pictureBox1.Size = new Size();
-            mqttClient = new MqttClient("test.mosquitto.org");
+            mqttClient = new MqttClient("172.30.4.44");
             mqttClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
             string clientId = Guid.NewGuid().ToString();
             mqttClient.Connect(clientId);
@@ -101,150 +101,164 @@ namespace SystemeLocalisation
         {
             foreach(KeyValuePair<int, BaliseData> kvp in d)
             {
-                Console.WriteLine("Key {0}\nTagId : {1} \nX : {2}\nY : {3}\nTimeStamp : {4} ", 
-                    kvp.Key, kvp.Value.tagId, kvp.Value.x, kvp.Value.y, kvp.Value.timestamp);
+                Console.WriteLine("Key {0}\nTagId : {1} \nX : {2}\nY : {3}\nTimeStamp : {4} \nSpeedX : {5} \nSpeedY : {6} ", 
+                    kvp.Key, kvp.Value.tagId, kvp.Value.x, kvp.Value.y, kvp.Value.timestamp, kvp.Value.SpeedX, kvp.Value.SpeedY);
             }
         }
 
         private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
-            // handle message received
-            var message = Encoding.UTF8.GetString(e.Message);
-            List<Result> result = JsonConvert.DeserializeObject<List<Result>>(message);
-
-            foreach (var item in result)
+            try
             {
-                if (item.tagId == mainId)
-                {
-                    Console.WriteLine("Ensuite ici on a modifie juste les valeur de la balise gps");
-                    gpsData.tagId = item.tagId;
-                    gpsData.x = item.data.coordinates.x;
-                    gpsData.y = item.data.coordinates.y;
-                    gpsData.timestamp = item.timestamp;
-                    gpsDataArchive = gpsData;
-                    //affichage_struct(stockage);
-                }
-                else if(item.tagId == secondId ||item.tagId == thirdId)
-                {
-                    if (stockage.ContainsKey(item.tagId))
-                    {
-                        Console.WriteLine("2eme fois qu'on recoit des data sur ce capteur");
-                        stockage[item.tagId].tagId = stockage_archive[item.tagId].tagId;
-                        stockage[item.tagId].isGet = stockage_archive[item.tagId].isGet;
-                        stockage[item.tagId].isLost = stockage_archive[item.tagId].isLost;
-                        stockage[item.tagId].x = item.data.coordinates.x;
-                        stockage[item.tagId].y = item.data.coordinates.y;
-                        stockage[item.tagId].timestamp = item.timestamp;
-                        Console.WriteLine("Affichage contenu stockage");
-                        affichage_struct(stockage);
-                        Console.WriteLine("Affichage contenu archive");
-                        affichage_struct(stockage_archive);
-                        decimal TimeStamp_Colis = stockage[item.tagId].timestamp - stockage_archive[item.tagId].timestamp;
-                        stockage[item.tagId].SpeedX = (Math.Abs(stockage_archive[item.tagId].x - stockage[item.tagId].x) / 1000) / TimeStamp_Colis;
-                        stockage[item.tagId].SpeedY = (Math.Abs(stockage_archive[item.tagId].y - stockage[item.tagId].y) / 1000) / TimeStamp_Colis;
-                        PackageIsCollected(item.tagId);
-                        PackageIsDropped(item.tagId);
-                    }
-                    else
-                    {
-                        BaliseData ajout = new BaliseData();
-                        BaliseData ajout_archive = new BaliseData();
-                        Console.WriteLine("First ici car au debut notre id existe pas");
-                        ajout.tagId = item.tagId;
-                        ajout.x = item.data.coordinates.x;
-                        ajout.y = item.data.coordinates.y;
-                        ajout.timestamp = item.timestamp;
-                        ajout.SpeedX = 0;
-                        ajout.SpeedY = 0;
-                        ajout.isGet = false;
-                        ajout.isLost = false;
 
-                        ajout_archive.tagId = item.tagId;
-                        ajout_archive.x = item.data.coordinates.x;
-                        ajout_archive.y = item.data.coordinates.y;
-                        ajout_archive.timestamp = item.timestamp;
-                        ajout_archive.SpeedX = 0;
-                        ajout_archive.SpeedY = 0;
-                        ajout_archive.isGet = false;
-                        ajout_archive.isLost = false;
-                        stockage.Add(item.tagId, ajout);
-                        stockage_archive.Add(item.tagId, ajout_archive);
-                        //Console.WriteLine("on affiche contenu de l'archive");
+                // handle message received
+
+                var message = Encoding.UTF8.GetString(e.Message);
+                List<Result> result = JsonConvert.DeserializeObject<List<Result>>(message);
+
+                foreach (var item in result)
+                {
+                    if (item.tagId == mainId)
+                    {
+                        //Console.WriteLine("Ensuite ici on a modifie juste les valeur de la balise gps");
+                        gpsData.tagId = item.tagId;
+                        gpsData.x = item.data.coordinates.x;
+                        gpsData.y = item.data.coordinates.y;
+                        gpsData.timestamp = item.timestamp;
+                        gpsDataArchive = gpsData;
+                        //affichage_struct(stockage);
+                    }
+                    else if(item.tagId == secondId ||item.tagId == thirdId)
+                    {
+                        if (stockage.ContainsKey(item.tagId))
+                        {
+                            //Console.WriteLine("2eme fois qu'on recoit des data sur ce capteur");
+                            stockage[item.tagId].tagId = stockage_archive[item.tagId].tagId;
+                            stockage[item.tagId].isGet = stockage_archive[item.tagId].isGet;
+                            stockage[item.tagId].isLost = stockage_archive[item.tagId].isLost;
+                            stockage[item.tagId].x = item.data.coordinates.x;
+                            stockage[item.tagId].y = item.data.coordinates.y;
+                            stockage[item.tagId].timestamp = item.timestamp;
+                            //Console.WriteLine("---------------------------------------------------------------------------------");
+                            //Console.WriteLine("Affichage contenu stockage");
+                            //affichage_struct(stockage);
+                            //Console.WriteLine("\n");
+                            //Console.WriteLine("Affichage contenu archive");
+                            //Console.WriteLine("\n");
+                            //affichage_struct(stockage_archive);
+                            //Console.WriteLine("Numero du tag {0}", item.tagId);
+                            decimal soustractionx = Math.Abs(stockage[item.tagId].x - stockage_archive[item.tagId].x);
+                            decimal soustractiony = Math.Abs(stockage[item.tagId].y - stockage_archive[item.tagId].y);
+                            decimal TimeStamp_Colis = Math.Abs(stockage[item.tagId].timestamp - stockage_archive[item.tagId].timestamp);
+                            //Console.WriteLine("Valeur en x : {0}", soustractionx);
+                            //Console.WriteLine("Valeur en y : {0}", soustractiony);
+                            //Console.WriteLine("Valeur time : {0}", TimeStamp_Colis);
+                            decimal speedx = (soustractionx / 1000) / TimeStamp_Colis;
+                            decimal speedy = (soustractiony / 1000) / TimeStamp_Colis;
+                            //Console.WriteLine("Valeur de la vitesse apres le calcul en x {0} et y {1} dans le stockage : ",speedx, speedy);
+                            stockage[item.tagId].SpeedX = speedx;
+                            stockage[item.tagId].SpeedY = speedy;
+                            //Console.WriteLine("Vitesse en x {0}  Vitesse en y {1} dans l'archive pour voir si on a bien mis les valeurs", stockage[item.tagId].SpeedX, stockage[item.tagId].SpeedY);
+                            PackageIsCollected(item.tagId);
+                            PackageIsDropped(item.tagId);
+                        }
+                        else
+                        {
+                            BaliseData ajout = new BaliseData();
+                            BaliseData ajout_archive = new BaliseData();
+                            //Console.WriteLine("First ici car au debut notre id existe pas");
+                            ajout.tagId = item.tagId;
+                            ajout.x = item.data.coordinates.x;
+                            ajout.y = item.data.coordinates.y;
+                            ajout.timestamp = item.timestamp;
+                            ajout.SpeedX = 0;
+                            ajout.SpeedY = 0;
+                            ajout.isGet = false;
+                            ajout.isLost = false;
+
+                            ajout_archive.tagId = item.tagId;
+                            ajout_archive.x = item.data.coordinates.x;
+                            ajout_archive.y = item.data.coordinates.y;
+                            ajout_archive.timestamp = item.timestamp;
+                            ajout_archive.SpeedX = 0;
+                            ajout_archive.SpeedY = 0;
+                            ajout_archive.isGet = false;
+                            ajout_archive.isLost = false;
+                            stockage.Add(item.tagId, ajout);
+                            stockage_archive.Add(item.tagId, ajout_archive);
+                        }
                     }
                 }
+            } catch(Exception ex)
+            {
+                //Console.WriteLine("NIQUEEEEE ZEBIIIIIIIIIIIIIII");
+                //Console.WriteLine(ex.Message);
             }
         }
 
         private void PackageIsCollected(int tagId)
         {
-            if (stockage.ContainsKey(tagId))
+            decimal TimeStampGps = gpsData.timestamp - gpsDataArchive.timestamp;
+            decimal TimeStamp_Colis = stockage[tagId].timestamp - stockage_archive[tagId].timestamp;
+            //Si capteur se deplace en x ou en y plus que la marge d'erreur alors le capteur de deplace
+            if(Math.Abs(stockage_archive[tagId].x - stockage[tagId].x)> 50 ||
+                Math.Abs(stockage_archive[tagId].y - stockage[tagId].y) > 50)
             {
-                decimal TimeStampGps = gpsData.timestamp - gpsDataArchive.timestamp;
-                decimal TimeStamp_Colis = stockage[tagId].timestamp - stockage_archive[tagId].timestamp;
-                //Si capteur se deplace en x ou en y plus que la marge d'erreur alors le capteur de deplace
-                if(Math.Abs(stockage_archive[tagId].x - stockage[tagId].x)> 210 ||
-                   Math.Abs(stockage_archive[tagId].y - stockage[tagId].y) > 210)
+                //Si la vitesse de deplacement est > 0.35m/s c'est pas un outlier mais bel est bien qmq qui deplace le colis
+                if((float)stockage[tagId].SpeedX > 3 || (float)stockage[tagId].SpeedY > 3)
                 {
-                    //Si la vitesse de deplacement est > 0.35m/s c'est pas un outlier mais bel est bien qmq qui deplace le colis
-                    if((float)stockage[tagId].SpeedX > 0.35 || (float)stockage[tagId].SpeedY > 0.35)
+                    //Console.WriteLine("Mouvement !!!!!!!!!!");
+                    //Si le colis se trouve à moins de 40 cm de moi alors c'est moi qui l'est recup
+                    if(Math.Abs(gpsData.x - stockage[tagId].x) < 400 &&
+                        Math.Abs(gpsData.y - stockage[tagId].y) < 400 && 
+                        TimeStamp_Colis < 2 && TimeStampGps < 2 && stockage[tagId].isGet == false)
                     {
-                        //Si le colis se trouve à moins de 40 cm de moi alors c'est moi qui l'est recup
-                        if(Math.Abs(gpsData.x - stockage[tagId].x) < 400 &&
-                           Math.Abs(gpsData.y - stockage[tagId].y) < 400 && 
-                           TimeStamp_Colis < 2 && TimeStampGps < 2 && stockage[tagId].isGet == false)
-                        {
-                            stockage[tagId].isGet = true;
-                            stockage[tagId].isLost = false;
-                            Console.WriteLine("---------------------------------------------------------------------------------");
-                            Console.WriteLine("Le colis : {0} a été recup par vous !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", tagId);
-                            Console.WriteLine("---------------------------------------------------------------------------------");
-                        }
+                        stockage[tagId].isGet = true;
+                        stockage[tagId].isLost = false;
+                        Console.WriteLine("---------------------------------------------------------------------------------");
+                        Console.WriteLine("Le colis : {0} a été recup par vous !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", tagId);
+                        Console.WriteLine("---------------------------------------------------------------------------------");
                     }
-                    //stockage_archive[tagId] = stockage[tagId];
-                    stockage_archive[tagId].tagId = stockage[tagId].tagId;
-                    stockage_archive[tagId].timestamp = stockage[tagId].timestamp;
-                    stockage_archive[tagId].x = stockage[tagId].x;
-                    stockage_archive[tagId].y = stockage[tagId].y;
-                    stockage_archive[tagId].isGet = stockage[tagId].isGet;
-                    stockage_archive[tagId].isLost = stockage[tagId].isLost;
-                    //Je sais pas si la ligne d'en dessous est correct :  tester et remplacer par celle de haut dessus
-                    //stockage_archive = stockage;
-                    gpsDataArchive = gpsData;
                 }
+                //stockage_archive[tagId] = stockage[tagId];
             }
+                stockage_archive[tagId].tagId = stockage[tagId].tagId;
+                stockage_archive[tagId].timestamp = stockage[tagId].timestamp;
+                stockage_archive[tagId].x = stockage[tagId].x;
+                stockage_archive[tagId].y = stockage[tagId].y;
+                stockage_archive[tagId].isGet = stockage[tagId].isGet;
+                stockage_archive[tagId].isLost = stockage[tagId].isLost;
+                //Je sais pas si la ligne d'en dessous est correct :  tester et remplacer par celle de haut dessus
+                //stockage_archive = stockage;
+                gpsDataArchive = gpsData;
         }
 
         private void PackageIsDropped(int tagId)
         {
-            if (stockage.ContainsKey(tagId))
+            //Si la distance entre moi et un colis est > 100 cm 
+            if(Math.Abs(gpsData.x - stockage[tagId].x) > 1000 ||
+                Math.Abs(gpsData.y - stockage[tagId].y) > 1000)
             {
-                //Si la distance entre moi et un colis est > 100 cm 
-                if(Math.Abs(gpsData.x - stockage[tagId].x) > 1000 ||
-                   Math.Abs(gpsData.y - stockage[tagId].y) > 1000)
+                if(stockage[tagId].isGet == true)
                 {
-                    if(stockage[tagId].isGet == true)
-                    {
-                        stockage[tagId].isGet = false;
-                        stockage[tagId].isLost = true;
+                    stockage[tagId].isGet = false;
+                    stockage[tagId].isLost = true;
 
-                        if(stockage[tagId].isLost==true)
-                        {
-                            Console.WriteLine("DEMI TOUR COLIS PERDU");
-                            //stockage_archive[tagId] = stockage[tagId];
-                            stockage_archive[tagId].tagId = stockage[tagId].tagId;
-                            stockage_archive[tagId].timestamp = stockage[tagId].timestamp;
-                            stockage_archive[tagId].x = stockage[tagId].x;
-                            stockage_archive[tagId].y = stockage[tagId].y;
-                            stockage_archive[tagId].isGet = stockage[tagId].isGet;
-                            stockage_archive[tagId].isLost = stockage[tagId].isLost;
-                            //Je sais pas si la ligne d'en dessous est correct :  tester et remplacer par celle de haut dessus
-                            //stockage_archive = stockage;
-                        }
+                    if(stockage[tagId].isLost==true)
+                    {
+                        Console.WriteLine("DEMI TOUR COLIS PERDU");
+                        //stockage_archive[tagId] = stockage[tagId];
+                        stockage_archive[tagId].tagId = stockage[tagId].tagId;
+                        stockage_archive[tagId].timestamp = stockage[tagId].timestamp;
+                        stockage_archive[tagId].x = stockage[tagId].x;
+                        stockage_archive[tagId].y = stockage[tagId].y;
+                        stockage_archive[tagId].isGet = stockage[tagId].isGet;
+                        stockage_archive[tagId].isLost = stockage[tagId].isLost;
+                        //Je sais pas si la ligne d'en dessous est correct :  tester et remplacer par celle de haut dessus
+                        //stockage_archive = stockage;
                     }
                 }
             }
         }
-
-
     }
 }
